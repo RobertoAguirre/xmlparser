e3c71ee75148d41889e506a94047b40d93bfe45c
RobertoAguirre <roberto.aguirre.rodriguez@gmail.com>
2023-12-04 10:26:59 -0600

    extraccion de datos xml

--- converter.js
+++ converter.js
@@ -15,8 +15,17 @@ parser.parseString(xml, (err, result) => {
 
     // `result` is a JavaScript object
     // Convert it to a JSON string
+    let version = JSON.stringify(result['cfdi:Comprobante']['$']['Version']);
+    let emisor = result['cfdi:Comprobante']['cfdi:Emisor'][0]['$'];
+    let rfc = emisor.Rfc;
+    let nombre = emisor.Nombre;
+    let regimenFiscal = emisor.RegimenFiscal;
+    console.log('RFC:', rfc);
+    console.log('Nombre:', nombre);
+    console.log('Regimen Fiscal:', regimenFiscal);
     const json = JSON.stringify(result, null, 4);
 
     // Log JSON or use it as needed
-    console.log(json);
+   // console.log(json);
+    
 });
